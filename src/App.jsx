f289e879d7355cc2c721d1a1c384b6e6e556949f
Ej <jaysolis697@gmail.com>
2026-01-03 11:29:29 +0800

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function App() {
+  return (
+    <div className="p-4 bg-gray-100">
+      <h1 className="text-3xl italic font-bold text-center text-blue-600 ">
+        Hello world!
+      </h1>
+    </div>
+  );
+}
+
+export default App;
